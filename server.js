314bb078dffcf3d109176b4f54037a2f8c5b7f01
Larson04 <lar23016@byui.edu>
2026-01-12 14:22:05 -0700

    start of 3 page site

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
 // Import express using ESM syntax
 import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+/**
+ * Declare Important Variables
+*/
+// Define the port number the server will listen on
+const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+/**
+ * Setup Express Server
+ */
 // Create an instance of an Express application
 const app = express();
 const name = process.env.NAME;
+
+/**
+ * Configure Express middleware
+ */
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+ * Declare Routes
+ */
 // Define a route handler for the root URL ('/')
 app.get('/', (req, res) => {
     res.send(`Hello, ${name}!`);
 });
-
-// Define the port number the server will listen on
-const PORT = 3000;
 
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
